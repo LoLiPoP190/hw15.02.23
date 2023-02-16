8ca14f3a065a469c165ccc67c3cbbe7461d9e471
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-02-16 22:53:24 +0600

    added solution for the first task

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1. Напишите функцию, которая выводит ваше имя и фамилию.
+// functionName("Erjan", "Kydyrov")  My  name is Erjan Kydyrov
+
+function printNameSurname(name, surname) {
+  console.log(`My name is ${name} ${surname}`);
+}
+printNameSurname("atak", "oskonbaev");
+
